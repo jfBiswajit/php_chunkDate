74bdff9a57442af78fd4d1e8e243c96bf08a347f
Biswajit Biswas <jfbiswajit@gmail.com>
2020-01-15 15:13:23 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+function chunkDate($dateStr) {
+        $createdAt=date_create($dateStr);
+        $today=date_create(date('Y-m-d'));
+        $diff=date_diff($createdAt, $today);
+
+        $days = $diff->days;
+        $month = floor($days / 30) ;
+        $year = floor($month / 12);
+
+        if ($days < 30) {
+          return $days . ' days ago';
+        } elseif ($month <= 12) {
+          return $month . ' month ago';
+        } else {
+          return $year . ' years ago';
+        }
+      }
+
+      echo chunkDate('2020-01-12');
